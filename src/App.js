0b6094b12086710747f7661047b87cea7927825e
logesaz <logeswaran@assistanz.com>
2022-08-05 11:45:20 +0000

    updated source file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Stackbill loves kubernetes
+          Welcome to assistanz loves kubernetes
         </p>
         <a
           className="App-link"
