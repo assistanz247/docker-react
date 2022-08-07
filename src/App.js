3cee410d58eb0e5844bcb99a9241a738bbd3cf0b
assistanz247 <logeswaran@assistanz.com>
2022-08-07 14:25:31 +0000

    demo for KD

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Assistanz loves kubernetes</p>
+        <p>KD loves kubernetes</p>
         <a
           className="App-link"
           href="https://reactjs.org"
