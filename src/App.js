5ced43278f6e9cdb53a85272c03fd1b53566fa85
assistanz247 <logeswaran@assistanz.com>
2022-08-08 04:50:36 +0000

    amal update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Our universe loves everyone</p>
+        <p>Amal Ranjith Loves Kubernetes</p>
         <a
           className="App-link"
           href="https://reactjs.org"
